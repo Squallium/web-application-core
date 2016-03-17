aeecced37dcee3fda31f8ea85b0658539e149feb
Borja Refoyo Ruiz <brefoyo@generagames.com>
2016-03-17 19:31:24 +0100

    primera petición a la api, de manera cutre

--- modules/diablo3/d3-api.js
+++ modules/diablo3/d3-api.js
@@ -4,10 +4,10 @@
 
 var auth = require('../../config/auth');
 var request = require('request');
+var util = require('util');
 
 
-
-var CAREER_URL = "htt;ps://%s.api.battle.net/d3/profile/%s/?locale=%s&apikey=%s"
+var CAREER_URL = "https://%s.api.battle.net/d3/profile/%s/?locale=%s&apikey=%s"
 
 module.exports = {
 
@@ -15,11 +15,13 @@ module.exports = {
      * EXAMPLE: https://eu.api.battle.net/d3/profile/squallium-2579/?locale=en_GB&apikey=appkey
      */
     careerProfile : function(zone, battleTag, locale) {
-        util.format(CAREER_URL, zone, battleTag, locale, auth.diablo3Auth.appKey);
+        var url = util.format(CAREER_URL, zone, battleTag, locale, auth.diablo3Auth.appKey);
 
-        request('http://www.google.com', function (error, response, body) {
+        request(url, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body) // Show the HTML for the Google homepage.
+            }else{
+                console.log("error del demonio");
             }
         });
     },
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var d3api = require('../modules/diablo3/d3-api');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,21 +9,50 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('users/register', {
-    title: 'Register'
-  });
+    res.render('users/register', {
+        title: 'Register'
+    });
+    next();
+}, function(req, res, next){
+    d3api.careerProfile("eu", "squallium-2579", "en_GB");
 });
 
-// process the signup form
-router.post('/signup', function (req, res) {
-  passport.authenticate('local-signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/',
-    failureFlash: true
-  })(req, res, function () {
-    console.log('Signup request finished.')
-  });
-});
+router.post('/signup',
+    function(req, res, next) {
+        passport.authenticate('local-signup', {
+                successRedirect: '/profile',
+                failureRedirect: '/login',
+                failureFlash: true
+            },
+
+            function () {
+                console.log("alsjkfañdjf");
+                next();
+            })(req, res, next)
+    }, function(req, res, next) {
+            console.log("omg omg");
+    });
+
+//app.get('/login', function(req, res, next) {
+//    passport.authenticate('local', function(err, user, info) {
+//        if (err) { return next(err); }
+//        if (!user) { return res.redirect('/login'); }
+//        req.logIn(user, function(err) {
+//            if (err) { return next(err); }
+//            return res.redirect('/users/' + user.username);
+//        });
+//    })(req, res, next);
+//}
+//
+//
+//);
+
+//router.post('/signup',
+//    passport.authenticate('local-signup', {
+//      successRedirect: '/profile',
+//      failureRedirect: '/login',
+//      failureFlash: true })
+//);
 
 router.get('/login', function (req, res, next) {
   res.render('users/login', {
